e3a04c8d0f4f4e20b927a189454d10a2b3d51b17
Daniela Patrício <dapatric@live.com.pt>
2019-08-06 22:49:07 +0100

    center shapes on slides

--- editpptx.py
+++ editpptx.py
@@ -7,6 +7,10 @@ for slide in prs.slides:
      for shape in slide.shapes:
          shape.height = Cm(15)
          shape.width = Cm(15)
+         shape.left = round((prs.slide_width - shape.width) / 2)
+         shape.top = round((prs.slide_height - shape.height) / 2)
+
+         print(prs.slide_height - shape.width /2)
 
 prs.save('testprs.pptx')
 
