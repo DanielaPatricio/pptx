08a5465bedb97f93a93485ba54d3136d0eea54e0
Daniela Patrício <dapatric@live.com.pt>
2019-08-09 22:03:57 +0100

    add 4:3 layout and resize images to it

--- editpptx.py
+++ editpptx.py
@@ -1,12 +1,14 @@
 from pptx import Presentation
-from pptx.util import Cm
+from pptx.util import Inches
 
 prs = Presentation('testprs.pptx')
 
 for slide in prs.slides:
-     for shape in slide.shapes:
-         shape.height = Cm(15)
-         shape.width = Cm(15)
+    prs.slide_width = Inches(10)
+    prs.slide_height = Inches(7.5)
+    for shape in slide.shapes:
+         shape.height = prs.slide_height
+         shape.width = prs.slide_width
          shape.left = round((prs.slide_width - shape.width) / 2)
          shape.top = round((prs.slide_height - shape.height) / 2)
 
