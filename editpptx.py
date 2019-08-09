cb9d5c76489d0aaacae0e7087cbe2ddf03148101
Daniela Patrício <dapatric@live.com.pt>
2019-08-09 22:33:25 +0100

    reformat code

--- editpptx.py
+++ editpptx.py
@@ -7,15 +7,11 @@ for slide in prs.slides:
     prs.slide_width = Inches(10)
     prs.slide_height = Inches(7.5)
     for shape in slide.shapes:
-         shape.height = prs.slide_height
-         shape.width = prs.slide_width
-         shape.left = round((prs.slide_width - shape.width) / 2)
-         shape.top = round((prs.slide_height - shape.height) / 2)
+        shape.height = prs.slide_height
+        shape.width = prs.slide_width
+        shape.left = round((prs.slide_width - shape.width) / 2)
+        shape.top = round((prs.slide_height - shape.height) / 2)
 
-         print(prs.slide_height - shape.width /2)
+        print(prs.slide_height - shape.width / 2)
 
 prs.save('testprs.pptx')
-
-
-
-
